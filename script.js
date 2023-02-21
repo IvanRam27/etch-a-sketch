5669c34a802921a871ca10466f5c04eb8b9d512c
Ivan <ivanmr2796@gmail.com>
2023-02-20 22:21:28 -0700

    add button to change grid, grid changes based on user prompt

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let columns = 16  //height
+let columns = 16 //height
 let rows = 16     //width
 
 function createGrid(height,width){
     for (let i = 0; i < width; i++) {        
-        const square = document.createElement("square");
+        const square = document.createElement("square"); 
         square.classList.add("box");
         document.getElementById("row").appendChild(square);
     }
@@ -13,14 +13,30 @@ function createGrid(height,width){
     const clone = p.cloneNode(true);
     document.getElementById("column").appendChild(clone);
     }
-}
 
-createGrid(columns,rows);
-
-const boxes = document.querySelectorAll(".box");
+    const boxes = document.querySelectorAll(".box");
 boxes.forEach(box => {
     box.addEventListener("mouseover", function handleClick(event) {
         box.setAttribute ("class", "hover")
     })
 })
+}
 
+createGrid(columns,rows); //starting grid
+
+
+
+function changeGrid(){  //function to create new grid when "change grid" button is clicked
+    const gones = document.querySelectorAll(".box"); //remove all empty boxes
+    gones.forEach(box => {
+        box.remove();
+    })
+    const goned = document.querySelectorAll(".hover"); //remove all hovered boxes
+    goned.forEach(hover => {
+        hover.remove();
+    })
+
+    columns = prompt("Height (Max. 100)")
+    rows = prompt("Width (Max. 100)")
+    createGrid(columns,rows); 
+}
