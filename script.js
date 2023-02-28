2a38fc0cd5f296e22cd5fe7d14ebe70c1ef21042
Ivan <ivanmr2796@gmail.com>
2023-02-27 22:42:53 -0700

    change grid deleting logic to eliminate empty rows

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 let size = 30
 
 function createGrid(gridSize){
-    for (let i = 0; i < gridSize; i++) {        
+
+    for (let i = 0; i < gridSize; i++) {        //append boxes to first row
+        const square = document.createElement("square"); 
+        square.classList.add("box");
+        document.getElementById("row").appendChild(square);
+    }
+    
+    for(let i= 1; i < gridSize; i++){
+
+        const row = document.createElement("row") //append empty rows to fill with boxes
+        row.id = "row";
+        document.getElementById("column").prepend(row)
+    
+    for (let i = 0; i < gridSize; i++) {        //append boxes to columns
         const square = document.createElement("square"); 
         square.classList.add("box");
         document.getElementById("row").appendChild(square);
     }
 
-    for(let i = 0; i < gridSize; i++){
-    const p = document.getElementById("row")
-    const clone = p.cloneNode(true);
-    document.getElementById("column").appendChild(clone);
+
     }
+
 
     const boxes = document.querySelectorAll(".box");
 boxes.forEach(box => {
@@ -26,14 +37,15 @@ createGrid(size); //starting grid
 
 
 function changeGrid(){  //function to create new grid when "change grid" button is clicked
-    const gones = document.querySelectorAll(".box"); //remove all empty boxes
+    const gones = document.querySelectorAll("#row"); //remove all empty boxes
     gones.forEach(box => {
         box.remove();
     })
-    const goned = document.querySelectorAll(".hover"); //remove all hovered boxes
-    goned.forEach(hover => {
-        hover.remove();
-    })
+
+    const newRow = document.createElement("newRow"); //create new row to start attaching boxes
+        newRow.id = "row"
+        document.getElementById("column").appendChild(newRow);
+ 
 
     size = prompt("Enter Grid Size (Max. 100)")
     while (isNaN(size) || size < 0 || size > 100) {
